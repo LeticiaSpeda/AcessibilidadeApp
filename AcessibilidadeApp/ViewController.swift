e7e0604235dbaa2984cf6b075a26101049a506ac
Leticia Speda <leticiaspeda@gmail.com>
2023-11-19 17:22:43 -0300

    feat: adicionado descricao para leitura da acessibilidade identificar textos e botoes

--- AcessibilidadeApp/ViewController.swift
+++ AcessibilidadeApp/ViewController.swift
@@ -7,13 +7,21 @@ final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view = viewScreen
+        setupAccesbility()
     }
 
     override func loadView() {
         viewScreen.delegate = self
     }
 
-    func setupAccesbility() {
+    private func setupAccesbility() {
+        setupReadAccesbility()
+        setupLabelAcessibility()
+        setupTraitsAcessibility()
+    }
+
+    private func setupReadAccesbility() {
+        viewScreen.titleLabel.isAccessibilityElement = true
         viewScreen.positionOneLabel.isAccessibilityElement = true
         viewScreen.positionTwoLabel.isAccessibilityElement = true
         viewScreen.positionThreeLabel.isAccessibilityElement = true
@@ -21,11 +29,31 @@ final class ViewController: UIViewController {
         viewScreen.positionFiveLabel.isAccessibilityElement = true
         viewScreen.loginButton.isAccessibilityElement = true
     }
+
+    private func setupLabelAcessibility() {
+        viewScreen.positionOneLabel.accessibilityLabel = "Posição um"
+        viewScreen.positionTwoLabel.accessibilityLabel = "Posição dois"
+        viewScreen.positionThreeLabel.accessibilityLabel = "Posição tres"
+        viewScreen.positionFourLabel.accessibilityLabel = "Posição quatro"
+        viewScreen.positionFiveLabel.accessibilityLabel = "Posição cinco"
+    }
+
+    private func setupTraitsAcessibility() {
+        viewScreen.positionOneLabel.accessibilityTraits = .staticText
+        viewScreen.positionTwoLabel.accessibilityTraits = .staticText
+        viewScreen.positionThreeLabel.accessibilityTraits = .staticText
+        viewScreen.positionFourLabel.accessibilityTraits = .staticText
+        viewScreen.positionFiveLabel.accessibilityTraits = .staticText
+        viewScreen.loginButton.accessibilityTraits = .button
+        viewScreen.titleLabel.accessibilityTraits = .header
+    }
+
+
 }
 
 extension ViewController: ViewScreenDelegate {
     func tappedLoginButton() {
-        
+
     }
 
 }
